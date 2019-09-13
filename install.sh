b8f70a209490ff8bd652470ff093b2f474797c4b
morten-b <32033374+morten-b@users.noreply.github.com>
2019-09-14 01:07:42 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -62,3 +62,66 @@ echo "tmpfs	/tmp	tmpfs	defaults,noatime,mode=1777	0	0" >> /mnt/etc/fstab
 
 echo -e "bash <(curl -S https://github.com/morten-b/archinstall/edit/master/chroot.sh)"
 echo -e "arch-chroot /mnt /bin/bash"
+
+# Setup system clock
+ln -sf /usr/share/zoneinfo/Europe/Copenhagen /etc/localtime
+hwclock --systohc --utc
+
+# Set the hostname
+echo thinkpad > /etc/hostname
+
+# Setting locale
+sed -i 's/#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/g' /etc/locale.gen
+sed -i 's/#en_DK.UTF-8 UTF-8/en_DK.UTF-8 UTF-8/g' /etc/locale.gen
+locale-gen
+
+# Update locale
+echo LANG=en_US.UTF-8 >> /etc/locale.conf
+echo LANGUAGE=en_US >> /etc/locale.conf
+echo LC_ALL=C >> /etc/locale.conf
+
+echo -e ">>  ROOT PASSWORD"
+# Set password for root
+passwd
+
+# Add real user remove -s flag if you don't whish to use zsh
+useradd -m -g users -G wheel -s /usr/bin/fish morten
+echo -e ">>  USER PASSWORD"
+passwd morten
+
+# Configure mkinitcpio with modules needed for the initrd image
+sed -i 's/^MODULES.*/MODULES=(ext4)/' /etc/mkinitcpio.conf
+sed -i 's/^HOOKS.*/HOOKS="base udev autodetect modconf block encrypt lvm2 filesystems keyboard fsck"/' /etc/mkinitcpio.conf
+
+# Regenerate initrd image
+mkinitcpio -p linux
+
+# Setup grub
+grub-install
+sed -i 's|^GRUB_CMDLINE_LINUX="".*|GRUB_CMDLINE_LINUX="cryptdevice=/dev/sda3:luks:allow-discards"|' /etc/default/grub
+grub-mkconfig -o /boot/grub/grub.cfg
+
+arch-chroot /mnt /bin/bash <<EOF
+# Enable sudo for user
+sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/g' /etc/sudoers
+
+# Make keyboard layout persistent
+# localectl set-x11-keymap dk
+
+# Install Yay
+pacman -Syu
+
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
+# Install yadm
+yay -S --noconfirm yadm-git
+
+# Pull settings from git
+yadm clone https://github.com/morten-b/dotfiles.git
+
+echo -e "bash <(curl -S https://raw.githubusercontent.com/morten-b/archinstall/master/post.sh)"
+echo -e "su - morten"
+echo -e "nano /etc/mkinitcpio.conf"
+EOF
